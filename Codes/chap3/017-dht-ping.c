c212dd60b8d69c3a9534c8ae48756894e71ff563
Paulo Cheadi Haddad Filho <paulochf@gmail.com>
2014-01-02 19:49:49 -0200

    reorganizando seção do DHT

--- Codes/chap3/017-dht-ping.c
+++ Codes/chap3/017-dht-ping.c
@@ -1,23 +1,18 @@
-// requisição de ping
 int send_ping(const struct sockaddr *sa, int salen, const unsigned char *tid, int tid_len) {
     int i = 0, rc; char buf[512]; // variáveis temporárias
 
-    rc = snprintf(buf + i, 512 - i, "d1:ad2:id20:");
-    INC(i, rc, 512);
+    rc = snprintf(buf + i, 512 - i, "d1:ad2:id20:"); INC(i, rc, 512);
     COPY(buf, i, myid, 20, 512); // cola o ID do peer, que está em "myid"
 
-    rc = snprintf(buf + i, 512 - i, "e1:q4:ping1:t%d:", tid_len);
-    INC(i, rc, 512);
+    rc = snprintf(buf + i, 512 - i, "e1:q4:ping1:t%d:", tid_len); INC(i, rc, 512);
     COPY(buf, i, tid, tid_len, 512); // cola o ID do Transmission de 4 bytes para a transação
     ADD_V(buf, i, 512);
 
-    rc = snprintf(buf + i, 512 - i, "1:y1:qe");
-    INC(i, rc, 512);
-    return dht_send(buf, i, 0, sa, salen); // envia o comando
+    rc = snprintf(buf + i, 512 - i, "1:y1:qe"); INC(i, rc, 512);
+    return dht_send(buf, i, 0, sa, salen); // envia a requisição de ping
     (...)
 }
 
-// resposta a ping
 int send_pong(const struct sockaddr *sa, int salen, const unsigned char *tid, int tid_len) {
     int i = 0, rc; char buf[512]; // variáveis temporárias
 
@@ -29,6 +24,6 @@ int send_pong(const struct sockaddr *sa, int salen, const unsigned char *tid, in
     ADD_V(buf, i, 512);
 
     rc = snprintf(buf + i, 512 - i, "1:y1:re"); INC(i, rc, 512);
-    return dht_send(buf, i, 0, sa, salen); // envia o comando
+    return dht_send(buf, i, 0, sa, salen); // envia a resposta a um ping
     (...)
 }
